ab367a87d63b4503103f606cfb225600c6e5dfca
jakubChudiak1 <jakubchudiak205@gmail.com>
2022-10-10 18:34:53 +0200

    modified:   resources/views/components/header.blade.php sss

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,23 @@
 <header>
     <div class="navbar hlavne-menu">
-        <div  class="container">
-            <h1>Menu PENIsS</h1>
+        <div  class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="logo">
+                        <img src="" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="menu-item">
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Login</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </header>

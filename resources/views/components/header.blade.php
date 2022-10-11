14387605fe556c256d9f7a6a199392c42af546e9
jakubChudiak1 <jakubchudiak205@gmail.com>
2022-10-11 13:57:55 +0200

    commit6

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,21 +1,23 @@
 <header id="header">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="menu">
-                    <img src="images/logo.png" alt="" class="logo">
-
-                    <div class="menu-options">
-                        <a href="">Home</a>
-                        <a href="">About Us</a>
-                        <a href="">Login</a>
-                    </div>
-
-                </div>
-
-
-            </div>
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <a class="navbar-brand" href="#"><img src="{{ asset("images/logo.png") }}" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon "></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Domov</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Details</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+          </ul>
         </div>
-
+      </nav>
     </div>
 </header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
 @extends('layout.index')
 @section('Content')
+<section id="home">
 <div class="row">
     <div class="col-lg-12">
 
-            <section id="home">
+
             <div class="home-banner">
                 <div class="col-lg-6">
                     <div class="banner-content">
@@ -17,10 +18,50 @@
                 </div>
 
             </div>
-        </section>
+
         </div>
     </div>
+</section>
 
+<section id="game-list">
+
+    <div class="container-fluid">
+
+        <div class="row justify-content-center">
+            <div class="col-lg-4 col-sm-6">
+                <div class=" game-item">
+                    <h1>Dota 2</h1>
+                    <img src="{{ asset("images/game-01.jpg") }}" alt="">
+                    <div class="game-content">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet minus quia eius non. Ratione optio quo veniam placeat ipsum! Placeat quisquam tempora ipsum officia dolores. A nam expedita maxime id.
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class=" game-item">
+                    <h1>Rust</h1>
+                    <img src="{{ asset("images/game-02.jpg") }}" alt="">
+                    <div class="game-content">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia deserunt voluptate animi, provident dolores harum. Similique placeat quidem distinctio illum modi asperiores perferendis animi aut,
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class=" game-item">
+                    <h1>Minecraft</h1>
+                    <img src="{{ asset("images/game-03.jpg") }}" alt="">
+                    <div class="game-content">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nobis temporibus a, provident quibusdam dolorem repellat pariatur voluptatum consequatur deleniti officiis in debitis doloribus ex repellendus delectus sed eos et!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div>
+
+
+
+</section>
 
 
 
